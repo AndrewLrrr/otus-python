0976a0efac13d337f824c9b820d1a2c7c16cd300
Andrey Larin <larin.projects@gmail.com>
2017-07-10 00:47:16 +0300

    Implemented all decorators in deco.py

--- hw1/deco.py
+++ hw1/deco.py
@@ -4,7 +4,7 @@
 from functools import update_wrapper
 
 
-def disable():
+def disable(func):
     '''
     Disable a decorator by re-assigning the decorator's name
     to this function. For example, to turn off memoization:
@@ -12,45 +12,62 @@ def disable():
     >>> memo = disable
 
     '''
-    return
+    return func
 
 
-def decorator():
+def decorator(deco):
     '''
     Decorate a decorator so that it inherits the docstrings
     and stuff from the function it's decorating.
     '''
-    return
+    def wrapper(func):
+        return update_wrapper(deco(func), func)
+    return update_wrapper(wrapper, deco)
 
 
-def countcalls():
+@decorator
+def countcalls(func):
     '''Decorator that counts calls made to the function decorated.'''
-    return
+    def wrapper(*args, **kwargs):
+        wrapper.calls += 1
+        return func(*args, **kwargs)
+    wrapper.calls = 0
+    return wrapper
 
 
-def memo():
+@decorator
+def memo(func):
     '''
     Memoize a function so that it caches all return values for
     faster future lookups.
     '''
-    return
+    def wrapper(*args, **kwargs):
+        update_wrapper(wrapper, func)
+        key = args, frozenset(kwargs.items()) if kwargs else args
+        if key in wrapper.cache:
+            return wrapper.cache[key]
+        res = wrapper.cache[key] = func(*args, **kwargs)
+        return res
+    wrapper.cache = {}
+    return wrapper
 
 
-def n_ary():
+@decorator
+def n_ary(func):
     '''
     Given binary function f(x, y), return an n_ary function such
     that f(x, y, z) = f(x, f(y,z)), etc. Also allow f(x) = x.
     '''
-    return
+    def wrapper(x, *args):
+        return x if not args else func(x, wrapper(*args))
+    return wrapper
 
 
-def trace():
+def trace(line):
     '''Trace calls made to function decorated.
-
     @trace("____")
     def fib(n):
         ....
-
     >>> fib(3)
      --> fib(3)
     ____ --> fib(2)
@@ -62,9 +79,23 @@ def trace():
     ____ --> fib(1)
     ____ <-- fib(1) == 1
      <-- fib(3) == 3
-
     '''
-    return
+    @decorator
+    def tracer(func):
+        def wrapper(*args, **kwargs):
+            arguments = tuple(list(args) + ['%s=%s' % (k, v) for k, v in kwargs.items()]) if kwargs else args
+            print '%s --> %s%s' % (line * wrapper.depth, func.__name__, arguments)
+            wrapper.depth += 1
+            res = func(*args, **kwargs)
+            wrapper.depth -= 1
+            print '%s <-- %s%s == %s' % (line * wrapper.depth, func.__name__, arguments, res)
+            return res
+        wrapper.depth = 0
+        return wrapper
+    return tracer
+
+# tests disable
+# memo = disable
 
 
 @memo
@@ -85,7 +116,14 @@ def bar(a, b):
 @trace("####")
 @memo
 def fib(n):
+    """Computes and prints the first n Fibonacci numbers."""
     return 1 if n <= 1 else fib(n-1) + fib(n-2)
+
+
+@trace("----")
+@memo
+def my_fn(a, c=1, b=2):
+    return c * (a + b)
 
 
 def main():
@@ -102,6 +140,16 @@ def main():
     print fib.__doc__
     fib(3)
     print fib.calls, 'calls made'
+
+    # my tests
+    # print 'bar(5, 4, 3, 2, 1)', bar(5, 4, 3, 2, 1)
+    # print my_fn(4, 5)
+    # print my_fn(4, b=5)
+    # print my_fn(4, b=5)
+    # print my_fn(3, c=10)
+    # print my_fn(3, b=5, c=10)
+    # print my_fn(3, c=10, b=5)
+    # print my_fn(4, 5)
 
 
 if __name__ == '__main__':
