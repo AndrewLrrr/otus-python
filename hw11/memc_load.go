ecbc0f6c026e008514122cc97da8cce9ffab06b0
Andrey Larin <larin.projects@gmail.com>
2017-10-22 20:26:30 +0300

    Fix bufLine func and add protobuf test

--- hw11/memc_load.go
+++ hw11/memc_load.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"github.com/golang/protobuf/proto"
-	"./appsinstalled"
+	"github.com/AndrewLrrr/memclog/appsinstalled"
 	"errors"
-	"fmt"
 	"os"
 	"strconv"
 	"strings"
-	"bufio"
 	"log"
+	"fmt"
 )
 
 type LogLine struct {
@@ -23,15 +22,20 @@ func dotRename(path string) error {
 	return os.Rename(path, newPath)
 }
 
-func bufLine(logLine LogLine) (string, appsinstalled.UserApps) {
-	data := appsinstalled.UserApps{
+func bufLine(logLine LogLine) (string, []byte, error) {
+	packed := appsinstalled.UserApps{
 		Apps: logLine.apps,
 		Lat:  &logLine.lat,
 		Lon:  &logLine.lon,
 	}
-	key := strings.Join([]string{logLine.devType, logLine.devId}, ":")
+	key := fmt.Sprintf("%s:%s", logLine.devType, logLine.devId)
 
-	return key, data
+	data, err := proto.Marshal(&packed)
+	if err != nil {
+		return "", data, errors.New(fmt.Sprintf("marshaling error: %s", err))
+	}
+
+	return key, data, nil
 }
 
 func (logLine *LogLine) parse(line string) error {
@@ -56,14 +60,14 @@ func (logLine *LogLine) parse(line string) error {
 		logLine.lat = lat
 	} else {
 		logLine.lat = 0
-		log.Printf("invalid latitude: `%s`", line)
+		log.Printf("invalid latitude: `%s`\n", line)
 	}
 
 	if lon, err := strconv.ParseFloat(lineParts[3], 64); err == nil {
 		logLine.lon = lon
 	} else {
 		logLine.lon = 0
-		log.Printf("invalid longitude: `%s`", line)
+		log.Printf("invalid longitude: `%s`\n", line)
 	}
 
 	apps := strings.Split(strings.TrimSpace(lineParts[4]), ",")
@@ -72,43 +76,12 @@ func (logLine *LogLine) parse(line string) error {
 		if num, err := strconv.ParseUint(app, 10, 32); err == nil {
 			logLine.apps = append(logLine.apps, uint32(num))
 		} else {
-			log.Printf("not all user apps are digits: `%s`", line)
+			log.Printf("not all user apps are digits: `%s`\n", line)
 		}
 	}
 
 	return nil
 }
 
-func protoTest() {
-	lines := "idfa\t1rfw452y52g2gq4g\t55.55\t42.42\t1423,43,567,3,7,23\ngaid\t7rfw452y52g2gq4g\t55.55\t42.42\t7423,424"
-
-	scanner := bufio.NewScanner(strings.NewReader(lines))
-	for scanner.Scan() {
-		logLine := LogLine{}
-		err := logLine.parse(scanner.Text())
-
-		if err != nil {
-			fmt.Printf("%s\n", err)
-		} else {
-			fmt.Print(logLine)
-			fmt.Print("\n")
-			_, packed := bufLine(logLine)
-			data, err := proto.Marshal(&packed)
-			if err != nil {
-				log.Fatal("marshaling error: ", err)
-			}
-			newTest := &appsinstalled.UserApps{}
-			err = proto.Unmarshal(data, newTest)
-			if err != nil {
-				log.Fatal("unmarshaling error: ", err)
-			}
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-}
-
 func main() {
-	protoTest()
 }
---
+++ hw11/memc_load_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"github.com/golang/protobuf/proto"
+	"github.com/AndrewLrrr/memclog/appsinstalled"
+	"bufio"
+	"strings"
+	"log"
+	"testing"
+)
+
+func TestBufLine(t *testing.T) {
+	lines := "idfa\t1rfw452y52g2gq4g\t55.55\t42.42\t1423,43,567,3,7,23\ngaid\t7rfw452y52g2gq4g\t56.55\t43.42\t7423,424"
+
+	scanner := bufio.NewScanner(strings.NewReader(lines))
+	for scanner.Scan() {
+		logLine := LogLine{}
+		err := logLine.parse(scanner.Text())
+
+		if err != nil {
+			t.Error(err)
+		} else {
+			key, test, err := bufLine(logLine)
+			if err != nil {
+				t.Error(err)
+			}
+			newTest := &appsinstalled.UserApps{}
+			err = proto.Unmarshal(test, newTest)
+
+			if err != nil {
+				t.Error("unmarshaling error: ", err)
+			}
+
+			if key != logLine.devType + ":" + logLine.devId {
+				t.Error("Incorrect key")
+			}
+
+			if logLine.lon != *newTest.Lon {
+				t.Error("Incorrect longitude")
+			}
+
+			if logLine.lat != *newTest.Lat {
+				t.Error("Incorrect latitude")
+			}
+
+			for i := range logLine.apps {
+				if logLine.apps[i] != newTest.Apps[i] {
+					t.Error("Incorrect apps")
+					break
+				}
+			}
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
